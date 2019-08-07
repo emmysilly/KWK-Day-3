ed48f781e50fb59030d7c0f547b2b1bd1fe86552
Emmy <EmmyChavez@Apples-MacBook-Pro-47.local>
2019-08-07 11:30:35 -0400

    Classes

---
+++ 07-classes&objects.playground/Contents.swift
@@ -0,0 +1,32 @@
+//Example of Class with
+
+class Can {
+    //store properties
+    let containerType = "Can"
+    let lidColor = "White"
+    let shippingContainer = "Boxrs"
+}
+
+var canPeaches = Can()
+
+print(canPeaches)
+print(canPeaches.lidColor)
+
+//1. Define a class called “Scholar”
+//2. Give the “Scholar” class at least 2  constant properties:
+//
+//grade of scholar is 10
+//scholar is studying Swift
+//name of scholar is Ophelia
+//3. Create an object called “nycScholar”
+//4. Print the “nycScholar” object
+
+class Scholar {
+    let grade = "10"
+    let scholar = "Swift"
+    let name = "Ophelia"
+}
+
+var nycScholar = Scholar()
+print(nycScholar)
+
